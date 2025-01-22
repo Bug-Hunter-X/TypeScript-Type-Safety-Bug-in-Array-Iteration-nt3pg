dcf576eb16cab9690b637af5ff7b506c87821acc
BugHunter-X <askzenai@gmail.com>
2025-01-22 11:54:06 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,9 @@
+function printAllNumbers(numbers: number[]): void {
+  for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+  }
+}
+
+const myNumbers: number[] = [1, 2, 3, 4, 5];
+
+printAllNumbers(myNumbers);
